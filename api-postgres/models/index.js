cdbedb582ac407e2df076593d1b056c2df6b5382
Alessito54 <arielpedraza10@outlook.com>
2026-04-15 23:34:49 -0600

    config de index en models

--- api-postgres/models/index.js
+++ api-postgres/models/index.js
@@ -11,11 +11,19 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  // Forzamos las dialectOptions aquí para evitar el error de certificado
+  sequelize = new Sequelize(process.env[config.use_env_variable], {
+    ...config,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  });
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
-
 fs
   .readdirSync(__dirname)
   .filter(file => {
